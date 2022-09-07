d5045d89667b3558c5a1ef014124fcae3302bf77
rick0x00 <rick.0x00@gmail.com>
2022-09-07 20:39:49 -0300

    add dependences

--- installgns3toubuntu.sh
+++ installgns3toubuntu.sh
@@ -8,5 +8,17 @@ sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install gns3-iou
 
+sudo apt-get install libpcap-dev
+git clone https://github.com/GNS3/ubridge.git
+cd ubridge
+make
+sudo make intall
+
 sudo usermod -a -G docker $USER
-sudo usermod -aG ubridge libvirt kvm wireshark $USER
+sudo usermod -a -G ubridge $USER 
+sudo usermod -a -G libvirt $USER 
+sudo usermod -a -G kvm $USER 
+sudo usermod -a -G wireshark $USER
+
+
+
